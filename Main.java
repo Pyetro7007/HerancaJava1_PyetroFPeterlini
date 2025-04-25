57180c4f650748646296110f304c41dc3984b64c
Pyetro7007 <pyetrofabricio2014@gmail.com>
2025-04-25 18:58:28 -0300

    Código finalizado, mas ainda poderia ser mais otimizado

--- Main.java
+++ Main.java
@@ -1,7 +1,32 @@
-// Superclasse
-class Funcionario {
-    void mostrarDados() {
-        System.out.println("Mostrando os dados do funcionário");
+import java.util.Scanner;
+
+// Classe Base
+class Pessoa {
+    String cpf;
+    String dataNascimento;
+
+    Pessoa(String cpf, String dataNascimento) { //Construtor da classe Pessoa
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
+    
+    void mostrarDadosPessoa() { //Exibir dados de Pessoa
+        System.out.println("CPF: " + cpf);
+        System.out.println("Data Nascimento: " + dataNascimento);
+    }
+}
+
+// Classe Funcionario herda classe base Pessoa
+class Funcionario extends Pessoa{
+    String nome;
+
+    Funcionario(String cpf, String dataNascimento, String nome) { //Construtor da classe Funcionario
+        super(cpf, dataNascimento); //Reutiliza da classe Funcionario
+        this.nome = nome;
+    }
+
+    void mostrarDadosFuncionario() { // Exibir dados funcionario
+        mostrarDadosPessoa();
     }
 
     void baterPonto() {
@@ -11,64 +36,153 @@ class Funcionario {
 
 // Subclasse Gerente
 class Gerente extends Funcionario {
+    Gerente(String cpf, String dataNascimento, String nome) { //Construtor da classe Gerente
+        super(cpf, dataNascimento, nome);
+    }
+
     @Override
-    void mostrarDados() {
-        System.out.println("Mostrando os dados do gerente");
+    void mostrarDadosFuncionario() {
+        System.out.println("\nNome do gerente: " + nome);
+        super.mostrarDadosFuncionario(); //Reutiliza da classe Funcionario
     }
 
     void realizarReuniao() {
-        System.out.println("Gerente está relizando reunião");
+        System.out.println(nome + " está relizando reunião");
     }
 }
 
 // Subclasse Desenvolvedor
 class Desenvolvedor extends Funcionario {
+    Desenvolvedor(String cpf, String dataNascimento, String nome) { //Construtor da classe Desenvolvedor
+        super(cpf, dataNascimento, nome);
+    }
+
     @Override
-    void mostrarDados() {
-        System.out.println("Mostrando os dados do desenvolvedor");
+    void mostrarDadosFuncionario() {
+        System.out.println("\nNome do desenvolvedor: " + nome);
+        super.mostrarDadosFuncionario(); //Reutiliza da classe Funcionario
     }
 
     void programar() {
-        System.out.println("O desenvolvedor está programando");
+        System.out.println(nome + " está programando");
     }
 }
 
 // Subclasse Estagiario
 class Estagiario extends Funcionario {
+    Estagiario(String cpf, String dataNascimento, String nome) { //Construtor da classe Estagiario
+        super(cpf, dataNascimento, nome); //Reutiliza da classe Funcionario
+    }
+
     @Override
-    void mostrarDados() {
-        System.out.println("Mostrando os dados do estagiário");
+    void mostrarDadosFuncionario() {
+        System.out.println("\nNome do estagiário: " + nome);
+        super.mostrarDadosFuncionario();
     }
 
     void fazerTarefa() {
-        System.out.println("O estagiário está realizando tarefas");
+        System.out.println(nome + " está realizando tarefas");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Gerente gerente = new Gerente();
-        Desenvolvedor desenvolvedor = new Desenvolvedor();
-        Estagiario estagiario = new Estagiario();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(" ");
+        Funcionario funcionario1 = null;
+        Funcionario funcionario2 = null;
+        Funcionario funcionario3 = null;
 
-        gerente.mostrarDados();
-        gerente.baterPonto();
-        gerente.realizarReuniao();
+        int op;
 
-        System.out.println(" ");
+        // Estrtura de repetição pro menu de opção
+        do {
+            System.out.println("\nEscolha uma opção:");
+            System.out.println("1 - Cadastrar novos funcionários (Só é possível cadastrar 3)"); //Case 1: Cadastra novos funcionários
+            System.out.println("2 - Exibir dados dos funcionários"); //Case 2: Exibe os dados cadastrados
+            System.out.println("3 - Sair"); //Case 3: Saída
 
-        desenvolvedor.mostrarDados();
-        desenvolvedor.baterPonto();
-        desenvolvedor.programar();
+            op = scanner.nextInt();
+            scanner.nextLine();
 
-        System.out.println(" ");
+            switch (op) {
+                case 1:
+                    System.out.println("\nDigite o nome do funcionário: ");
+                    String nome = scanner.nextLine();
 
-        estagiario.mostrarDados();
-        estagiario.baterPonto();
-        estagiario.fazerTarefa();
+                    System.out.println("\nDigite o cpf do funcionário: ");
+                    String cpf = scanner.nextLine();
 
-        System.out.println(" ");
+                    System.out.println("\nDigite a data de nascimento: ");
+                    String dataNascimento = scanner.nextLine();
+
+                    System.out.println("Escolha o tipo de funcionário: ");
+                    System.out.println("1 - Gerente: ");
+                    System.out.println("2 - Desenvolvedor: ");
+                    System.out.println("3 - Estagiário: ");
+
+                    int tipo = scanner.nextInt();
+                    scanner.nextLine();
+
+                    Funcionario novoFuncionario = null;
+
+                    switch (tipo) { // Decidir o tipo dos funcionários
+                        case 1:
+                            novoFuncionario = new Gerente(cpf, dataNascimento, nome);
+                        break;
+
+                        case 2:
+                            novoFuncionario = new Desenvolvedor(cpf, dataNascimento, nome);
+                        break;
+
+                        case 3:
+                            novoFuncionario = new Estagiario(cpf, dataNascimento, nome);
+                        break;
+
+                        default:
+                            novoFuncionario = null;
+                            System.out.println("\nCargo inválido. Funcionário não cadastrado");
+                        break;
+                    }
+
+                    if (novoFuncionario != null) { //Armazena os funcionários 
+                        if (funcionario1 == null) {
+                            funcionario1 = novoFuncionario;
+                        } else if (funcionario2 == null) {
+                            funcionario2 = novoFuncionario;
+                        } else if (funcionario3 == null) {
+                            funcionario3 = novoFuncionario;
+                        } else {
+                            System.out.println("\nLimite atingido de funcionário cadastrados");
+                        }
+                    }
+                break;
+
+                case 2:
+                    System.out.println("\nExibindo funcionários"); //Exibe os funcionários
+                    if (funcionario1 != null) {
+                        funcionario1.mostrarDadosFuncionario();
+                    }
+                    if (funcionario2 != null) {
+                        funcionario2.mostrarDadosFuncionario();
+                    }
+                    if (funcionario3 != null) {
+                        funcionario3.mostrarDadosFuncionario();
+                    }
+                    if (funcionario1 == null && funcionario2 == null && funcionario3 == null) {
+                        System.out.println("Nenhum funcionário está cadastrado.");
+                    }
+                break;
+
+                case 3:
+                    System.out.println("\nVocê saiu do programa");
+                break;
+
+                default: //Caso o usuário digite uma opção inválida
+                    System.out.println("\nOpção inválida, tente novamente");
+                break;
+            }
+        } while(op != 3); //Fecha o do
+        scanner.close(); //Fecha o Scanner
     }
 }
